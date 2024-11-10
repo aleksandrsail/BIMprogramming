12f0aae4e2e524ab4cbae69a32be4dbfde6449db
aleksandrsail <aleksandr.subbota02@gmail.com>
2024-11-10 11:34:11 +0300

    L1_task2_fixed.py

--- Лабораторная работа 1/L1_task2.py
+++ Лабораторная работа 1/L1_task2.py
@@ -1,16 +1,16 @@
 # Вводим исходные данные о дискете и книгах (случайные числа глав, строк и символов в строке, тк нет информации о том,
 # какие книги)
 disk_Mb = 1.44
-chapters = 30
-strings = 200
-symbols_in_string = 30
-symbol_b = 1
+pages = 100
+strings = 50
+symbols_in_string = 25
+symbol_b = 4
 
 # Пересчитываем объём диска в байты
 disk_b = disk_Mb * 1024 ** 2
 
 # Находим объём одной условной книги
-book_b = symbol_b * symbols_in_string * strings * chapters
+book_b = symbol_b * symbols_in_string * strings * pages
 
 # TODO Найдите количество книг, которое можно разместить на дискете
 
